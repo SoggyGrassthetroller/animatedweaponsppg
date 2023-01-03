1181188d3b2b0b4cde8bd8a8890ce37c1e8ef943
based soggy grass man <121841325+SoggyGrassthetroller@users.noreply.github.com>
2023-01-02 22:30:59 -0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -9,42 +9,40 @@ namespace AnimatedWeapons
         {
 
             ModAPI.Register(
-                new Modification()
-                {
-                    OriginalItem = ModAPI.FindSpawnable("Pistol"), // you can also do m1 garand or rifle.
-                    NameOverride = "animated handgun",
-                    NameToOrderByOverride = "!",
+				new Modification()
+				{
+                    OriginalItem = ModAPI.FindSpawnable("Pistol"), //can be switched out for «rifle»
+                    NameOverride = "pistol",
                     DescriptionOverride = "",
                     CategoryOverride = ModAPI.FindCategory("Firearms"),
-                    ThumbnailOverride = ModAPI.LoadSprite("pistol.png"),
+                    ThumbnailOverride = ModAPI.LoadSprite("thumbnail.png"),
                     AfterSpawn = (Instance) =>
                     {
                         ModAPI.KeepExtraObjects();
+                        var ColtSlide = Instance.transform.Find("Slide");
+                        ColtSlide.GetComponent<SpriteRenderer>().sprite = ModAPI.LoadSprite("slide.png",);
 
-                        var MakarovSlide = Instance.transform.Find("Slide");
-                        MakarovSlide.GetComponent<SpriteRenderer>().sprite = ModAPI.LoadSprite("pistolslide.png");
-
-                        Instance.GetComponent<SpriteRenderer>().sprite = ModAPI.LoadSprite("pistolCol.png", 10);
+                        Instance.GetComponent<SpriteRenderer>().sprite = ModAPI.LoadSprite("gun.png",); //for this do not include the sprite for the slide just the base gun.
+                        Instance.GetComponent<FirearmBehaviour>().barrelPosition = new Vector2(0.2f, 0.086f);
                         foreach (var c in Instance.GetComponents<Collider2D>())
                         {
                             GameObject.Destroy(c);
 						}
                         Instance.FixColliders();
-                        Instance.GetComponent<SpriteRenderer>().sprite = ModAPI.LoadSprite("Textures/Makarov.png");
+                        Instance.GetComponent<FirearmBehaviour>().BulletsPerShot = 1;
 
                         var firearm = Instance.GetComponent<FirearmBehaviour>();
-                        firearm.BulletsPerShot = 1;
-                        firearm.barrelPosition = new Vector2(0.15f, 0.08f);
+
                         firearm.ShotSounds = new AudioClip[]
                         {
-                              ModAPI.LoadSound(""),
+                ModAPI.LoadSound(""), //import mp3 or wav files here.
                         };
 
-                        Cartridge customCartridge = ModAPI.FindCartridge("9mm");
-                        customCartridge.name = "";
-                        customCartridge.Damage = 5f;
+                        Cartridge customCartridge = ModAPI.FindCartridge("9.9x19mm");
+                        customCartridge.name = "gun";
+                        customCartridge.Damage = 4f;
                         customCartridge.Recoil = 0.1f;
-                        customCartridge.ImpactForce = 0.1f;
+                        customCartridge.ImpactForce = 0.2f;
                         firearm.Cartridge = customCartridge;
 		    }
                 }
